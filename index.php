39334722adb2315307320b50deea6aba4765c3bc
Inn0 <daan.brocatus@outlook.com>
2017-03-18 22:20:38 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>StelLAN</title>
+  </head>
+  <body>
+    <script src="js/jquery-3.1.1.js" type="text/javascript"></script>
+    <script src="js/bootstrap.min.js" type="text/javascript"></script>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+  </body>
+</html>
