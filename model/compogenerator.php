65e0ec34c90a3c8d1f0a7974b53e14038a558242
Youri Ben Hopman <Youri Ben Hopman>
2017-03-19 22:02:02 +0100

    compogenerator.php & clan_ar.php added

---
+++ model/compogenerator.php
@@ -0,0 +1,9 @@
+<?php
+class compogenerator{
+  function __construct($clan_ar, $game){
+    //I'll leave the technical stuff for later
+    $clanAmount = count($clan_ar);
+    $botAmount = 16 - $clanAmount;
+    
+  }
+}
