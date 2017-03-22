279fd83156c59b5288c782cd54f5fa5d5ae5d9c1
Youri Hopman <youri.hopman@outlook.com>
2017-03-22 23:19:35 +0100

    Compogenerator updated. Pools can now be made.

--- model/compogenerator.php
+++ model/compogenerator.php
@@ -2,8 +2,11 @@
 class compogenerator{
   function __construct($clan_ar, $game){
     //I'll leave the technical stuff for later
-    $clanAmount = count($clan_ar);
-    $botAmount = 16 - $clanAmount;
+    $bot_ar = array(BOT, BOT, BOT, BOT, BOT, BOT, BOT, BOT, BOT, BOT, BOT, BOT, BOT, BOT, BOT, BOT);
+    $poolable_ar = array_replace($bot_ar, $clan_ar);
+    shuffle($poolable_ar);
+    $pool1_ar = array_slice($poolable_ar, 0, 8);
+    $pool2_ar = array_slice($poolable_ar, 8, 8);
     
   }
 }
